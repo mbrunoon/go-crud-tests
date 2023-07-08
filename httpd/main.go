772005cbe097d98df9aea8b44f7c70e5a9ba08e2
mbrunoon <contato@marcosbruno.com>
2023-07-08 11:20:55 +0100

    feat: create get and post newsfeed endpoint

--- httpd/main.go
+++ httpd/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"newsfeeder/httpd/handler"
+	"newsfeeder/plataform/newsfeed"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	feed := newsfeed.New()
 	r := gin.Default()
 
 	r.GET("/ping", handler.PingGet())
+	r.GET("/newsfeed", handler.NewsfeedGet(feed))
+	r.POST("/newsfeed", handler.NewsfeedPost(feed))
 
 	r.Run()
 }
